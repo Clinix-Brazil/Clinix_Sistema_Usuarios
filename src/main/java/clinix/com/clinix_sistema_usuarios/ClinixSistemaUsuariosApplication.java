52c546f75a27e59d8bf7938eba924c379d756e65
AllanSmithll <allanammancio2004@gmail.com>
2025-03-04 00:25:33 -0300

    feat: connection by RMI completed

--- src/main/java/clinix/com/clinix_sistema_usuarios/ClinixSistemaUsuariosApplication.java
+++ src/main/java/clinix/com/clinix_sistema_usuarios/ClinixSistemaUsuariosApplication.java
@@ -1,12 +1,25 @@
 package clinix.com.clinix_sistema_usuarios;
 
+import clinix.com.clinix_sistema_usuarios.rmi.UsuarioServiceImpl;
+import com.clinix.api.interfaces.UsuarioService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 @SpringBootApplication
 public class ClinixSistemaUsuariosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClinixSistemaUsuariosApplication.class, args);
+        try {
+            Registry registry = LocateRegistry.createRegistry(1099);
+            UsuarioService usuarioService = new UsuarioServiceImpl();
+            registry.rebind("UsuarioService", usuarioService);
+            System.out.println("Serviço RMI de Usuários registrado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
