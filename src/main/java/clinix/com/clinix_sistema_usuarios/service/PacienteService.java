fc47aabf7166d5b08443d49d123277038b3014d4
Amandacdev <amanda.crz.aj@gmail.com>
2025-03-12 22:11:34 -0300

    Ajustes mensageria

--- src/main/java/clinix/com/clinix_sistema_usuarios/service/PacienteService.java
+++ src/main/java/clinix/com/clinix_sistema_usuarios/service/PacienteService.java
@@ -1,22 +1,31 @@
 package clinix.com.clinix_sistema_usuarios.service;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import clinix.com.clinix_sistema_usuarios.dto.MedicoRmiDTO;
 import clinix.com.clinix_sistema_usuarios.dto.PacienteRmiDTO;
 import clinix.com.clinix_sistema_usuarios.model.Paciente;
 import clinix.com.clinix_sistema_usuarios.repository.PacienteRepository;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static org.springframework.amqp.rabbit.support.micrometer.RabbitTemplateObservation.TemplateLowCardinalityTags.ROUTING_KEY;
 
 @Service
 public class PacienteService {
 
     private final PacienteRepository pacienteRepository;
 
+    private final RabbitTemplate rabbitTemplate;
+    private static final String EXCHANGE_NAME = "cadastro.exchange";
+    private static final String ROUTING_KEY = "cadastro.novo";
+
     @Autowired
-    public PacienteService(PacienteRepository pacienteRepository){
+    public PacienteService(PacienteRepository pacienteRepository, RabbitTemplate rabbitTemplate){
         this.pacienteRepository = pacienteRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public List<Paciente> listarTodos() {
@@ -33,6 +42,10 @@ public class PacienteService {
 
     public Paciente salvar(Paciente paciente) {
         return this.pacienteRepository.save(paciente);
+    }
+
+    public void enviarParaFila(Paciente paciente) {
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, paciente);
     }
 
     public Paciente atualizar(Paciente pacienteAtualizado) {
