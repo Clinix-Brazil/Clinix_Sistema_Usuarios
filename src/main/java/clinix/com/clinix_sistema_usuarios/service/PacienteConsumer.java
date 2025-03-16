3362a543496bbd8c878316a2582b1b52efbf1ac6
Amandacdev <amanda.crz.aj@gmail.com>
2025-03-16 16:38:47 -0300

    Finalizando msg paciente

--- src/main/java/clinix/com/clinix_sistema_usuarios/service/PacienteConsumer.java
+++ src/main/java/clinix/com/clinix_sistema_usuarios/service/PacienteConsumer.java
@@ -1,9 +1,11 @@
 package clinix.com.clinix_sistema_usuarios.service;
 
-import clinix.com.clinix_sistema_usuarios.model.Paciente;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import clinix.com.clinix_sistema_usuarios.model.Paciente;
+import org.springframework.beans.factory.annotation.Autowired;
 
 //Consumidor - fila - mensageria
 @Service
@@ -11,17 +13,17 @@ public class PacienteConsumer {
 
     private final PacienteService pacienteService;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Autowired
     public PacienteConsumer(PacienteService pacienteService) {
         this.pacienteService = pacienteService;
     }
 
-    @RabbitListener(queues = "fila_pacientes")
+    @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void consumirPacienteDaFila(Paciente paciente) {
-        System.out.println("📥 Mensagem recebida na fila: " + paciente);
-
+        System.out.println("📥 Mensagem recebida da fila: " + paciente);
         pacienteService.salvar(paciente);
-
         System.out.println("✅ Paciente cadastrado no banco: " + paciente.getNome());
     }
 }
